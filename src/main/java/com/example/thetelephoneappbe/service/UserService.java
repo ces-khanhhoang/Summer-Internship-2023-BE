d238e6d1fb373b607972fc42fe86163fe9030623
noo <ta642379@gmail.com>
2023-06-21 17:44:25 +0700

    Connect with fe via websocket
    update feat/join message

--- src/main/java/com/example/thetelephoneappbe/service/UserService.java
+++ src/main/java/com/example/thetelephoneappbe/service/UserService.java
@@ -2,6 +2,11 @@ package com.example.thetelephoneappbe.service;
 
 import com.example.thetelephoneappbe.model.User;
 
+import java.util.List;
+
 public interface UserService {
-    User creatUser(User user);
+    User creatUser(String userName, RoomService roomService, RoleService roleService);
+    void joinUser(Long idRoom, String userName, RoomService roomService, RoleService roleService);
+    List<User> getUserByIdRoom(Long idRoom);
+
 }
