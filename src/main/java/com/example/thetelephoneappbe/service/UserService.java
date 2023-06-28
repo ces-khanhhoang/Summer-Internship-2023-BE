f35a0db3e89e527bec88c4324bc7ad96778fe34b
qnguyen <nguyenquan.212002@gmail.com>
2023-06-28 13:49:50 +0700

    Create api remove user

--- src/main/java/com/example/thetelephoneappbe/service/UserService.java
+++ src/main/java/com/example/thetelephoneappbe/service/UserService.java
@@ -8,6 +8,7 @@ public interface UserService {
     User creatUser(String userName, RoomService roomService, RoleService roleService);
     void joinUser(Long idRoom, String userName, RoomService roomService, RoleService roleService);
     List<User> getUserByIdRoom(Long idRoom);
+    public User saveUser(User user);
 
 
 }
